f616c37355109b796fbd30016611188012ef12fc
Piriya <piriyalapa@gmail.com>
2024-01-11 22:13:59 +0700

    update to try to deploy

---
+++ src/utilities.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    fistColor:'#666CA3',
+    secondColor:"#13183F",
+    thirdColor:"#83869A",
+    fourthColor:"#F74780",
+    fifthColor:"#FFA7C3",
+    sixthColor:"#FFFFFF",
+    sventhColor:"linear-gradient(180deg, #FF6F48 0.04%, #F02AA6 100%)",
+    eighthColor:"linear-gradient(180deg, #4851FF 0%, #F02AA6 99.92%)"
+}
