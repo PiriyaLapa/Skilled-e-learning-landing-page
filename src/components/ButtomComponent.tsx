4fa933005535b45b67e059b3040c1389aafe3716
Piriya <piriyalapa@gmail.com>
2024-01-13 21:54:31 +0700

    I tried to create btn component

--- src/components/ButtomComponent.tsx
+++ src/components/ButtomComponent.tsx
@@ -1,4 +1,16 @@
 import {Component, ReactNode} from 'react';
 import styled from 'styled-components';
 
+interface PrimaryBtnProps{
+
+}
+
+export default class PrimaryBtn extends Component<PrimarnyBtnProps>{
+    render():ReactNode{
+        return(
+            <StyledPrimaryBtn></StyledPrimaryBtn>
+        )
+    }
+}
+
 
--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,18 +1,6 @@
 import { Component, ReactNode } from "react";
-import styled from "styled-components";
-import { theme } from "../../utilities";
+import { StyledNavbar } from "../../style/styledComponent";
 
-const StyledNavbar = styled.div<{ theme?: typeof theme }>`
-  @media screen and (min-width: 375px) {
-    width: 80%;
-    height: auto;
-    border: 1px solid yellow;
-    text-align: center;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-  }
-`;
 
 export class Navbar extends Component {
   render(): ReactNode {
