9f9bf8533be466d8de6c98e035c0702e1b7cf3e2
PiriyaLapa <piriyalapa@gmail.com>
2024-01-14 12:23:33 +0700

    update and refector code

--- src/style/styledComponent.tsx
+++ src/style/styledComponent.tsx
@@ -31,13 +31,19 @@ export const StyledPrimaryBtn = styled.button<{ theme: typeof theme }>`
     width: 140px;
     color:white;
     font-weight:600;
+    position:relative;
+    right:0;
+    top:50%;
+    transform:translateY(16%);
   }
 `;
 
 // StyleLogo
-export const StyledLogo = styled.span`
+export const StyledLogo = styled.h1`
   font-size:28px; 
   font-style:normall;
   font-weight:800;
   line-height:normall;
+  text-align:center;
+  display:relative;
 `;
