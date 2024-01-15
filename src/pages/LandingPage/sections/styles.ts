725cbcab1d4c714a808e8bb20d8a81c919aaab37
Piriya <piriyalapa@gmail.com>
2024-01-15 13:58:50 +0700

    updastery

--- src/pages/LandingPage/sections/styles.ts
+++ src/pages/LandingPage/sections/styles.ts
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.div`
-    background-color:none;
+  background-color: none;
 `;
 
-
 export const StyledHero = styled.div`
-    background:none;
+  @media screen and (min-width: 375px) {
+    :nth-child(3) {
+      float: left;
+        margin-left: 5vw;
+        margin-top: 1vh;
+    }
+  }
 `;
