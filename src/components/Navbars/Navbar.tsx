18d98f67f2793ddcf42a3ace81af952730f20111
Piriya <piriyalapa@gmail.com>
2024-01-15 13:43:59 +0700

    try to deploy

--- src/components/Navbars/Navbar.tsx
+++ src/components/Navbars/Navbar.tsx
@@ -3,14 +3,16 @@ import { StyledNavbar } from "./style";
 import PrimaryBtn from "../Buttons/PrimaryBtn";
 import Logo from "../Logos/Logo";
 import { HeaderContent } from "../../apis/apis";
+import { theme } from "../../utilities";
 
 export class Navbar extends Component {
   render(): ReactNode {
     const { logoContent, btnContent } = HeaderContent;
+    const  btnColor  = theme.secondColor;
     return (
       <StyledNavbar>
         <Logo message={logoContent} />
-        <PrimaryBtn message={btnContent} />
+        <PrimaryBtn message={btnContent} color={btnColor} />
       </StyledNavbar>
     );
   }
