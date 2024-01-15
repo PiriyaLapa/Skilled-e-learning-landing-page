258de172bda8d6e8e90bf1613448caa5f1c181c7
Piriya <piriyalapa@gmail.com>
2024-01-15 10:46:43 +0700

    update color and try to deploy

--- src/pages/LandingPage/sections/Hero.tsx
+++ src/pages/LandingPage/sections/Hero.tsx
@@ -2,7 +2,6 @@ import { Component, ReactNode } from "react";
 import { HeaderContent } from "../../../apis/apis";
 import HeadLine from "../../../components/HeadLine/HeadLine";
 import HeadContent from "../../../components/HeadContent/HeadContent";
-import { theme } from "../../../utilities";
 
 export default class Hero extends Component {
   render(): ReactNode {
