fc1d5ff18fe327b7b36f198aab28a1a27ffa22db
Piriya <piriyalapa@gmail.com>
2024-01-17 17:15:47 +0700

    update to try to deplo

--- src/components/figure/styles.ts
+++ src/components/figure/styles.ts
@@ -1,6 +1,19 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledFigure = styled.img`
-   width:100%; 
-   height:auto;
-`
+  /* Mobile screen */
+  @media screen and (min-width: 375px) {
+    width: 100%;
+    heigth: auto;
+  }
+
+  /* Tablet screen  */
+  @media screen and (min-width: 768px) {
+    width: 90%;
+    position:absolute;
+    top:50%;
+    left:50%;
+    transform:translate(10%,-50%);
+    overflow:hidden;
+  }
+`;
--- src/pages/LandingPage/sections/styles.ts
+++ src/pages/LandingPage/sections/styles.ts
@@ -5,6 +5,7 @@ export const StyledHeader = styled.div`
 `;
 
 export const StyledHero = styled.div`
+  overflow:hidden;
 
   // Mobile screen
   @media screen and (min-width: 375px) {
@@ -16,21 +17,24 @@ export const StyledHero = styled.div`
 
     // Tablat screen
     @media screen and (min-width:768px){
-        overflow:hidden;
-      
-      :nth-child(2){
-        width:30%; 
-      }
+
     }  
   }
 `;
 
 export const ContentBox = styled.div`
-
+  width:100%;
 // Tablet screen
   border:1px solid red;
   @media screen and (min-width: 768px) {
+    width:60%;
     padding: 0 15px;
+    :nth-child(1){
+      width:90%;
+    }
+    :nth-child(2){
+
+    }
   }
 `;
 
